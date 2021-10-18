e6903b92c85f5a532223cf7450b73700f5280cc9
eugen <demontage09@rambler.ru>
2021-10-18 21:38:27 +0500

    добавлен декоратор vsearch

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # save this as app.py
 from flask import Flask
-
+from vsearch import search4letters
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "I'm is Developer for 1000000$ !"
+
+@app.route('/search4')
+def do_search() -> str:
+    return str(search4letters('life, the univers, and everything', 'eiru,!'))
 app.run()
