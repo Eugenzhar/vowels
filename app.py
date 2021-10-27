c5c9f7cb1fa6973b1a3d4c1b3cb1f3e92fb5be32
eugen <demontage09@rambler.ru>
2021-10-27 19:42:10 +0500

    добавлен vsearc.log, но нет вывода запросов

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
+
 
 @app.route("/")
 def hello():
@@ -16,6 +20,7 @@ def do_search() -> str:
     letters = request.form['letters']
     title = 'Here are your results: '
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
                            the_phrase = phrase,
                            the_letters = letters,
@@ -26,8 +31,10 @@ def do_search() -> str:
 def entry_page() ->'html':
     return render_template('entry.html', the_title='Welcome to search for letters on the web!')
 
-# def log_request(req: 'flask_request', res: str) -> None:
-#     with open()
-
+@app.route("/viewlog")
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return contents
 if __name__ == '__main__':
     app.run(debug=True)
